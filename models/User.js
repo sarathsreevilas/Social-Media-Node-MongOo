ec374e147ddca7ac281affcd92274022bbf6f298
smug <supersarath007@gmail.com>
2022-11-22 09:01:07 +0530

    initial

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { default: mongoose } = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      min: 7,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      defalult: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
